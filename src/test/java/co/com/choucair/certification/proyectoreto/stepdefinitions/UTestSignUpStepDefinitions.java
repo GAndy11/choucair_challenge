8e8a28ecb2180bff36095d584481581137d4a4b7
Alexander Echeverry Torres <alexanderecheverry11@gmail.com>
2021-01-18 21:09:31 -0500

    Implementación de .feature y del stepDefinition

--- src/test/java/co/com/choucair/certification/proyectoreto/stepdefinitions/UTestSignUpStepDefinitions.java
+++ src/test/java/co/com/choucair/certification/proyectoreto/stepdefinitions/UTestSignUpStepDefinitions.java
@@ -1,4 +1,23 @@
 package co.com.choucair.certification.proyectoreto.stepdefinitions;
 
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
 public class UTestSignUpStepDefinitions {
+
+    @Given("^than Alexander wants to create account in uTest\\.com$")
+    public void thanAlexanderWantsToCreateAccountInUTestCom() {
+
+    }
+
+    @When("^he fill the data required for the page$")
+    public void heFillTheDataRequiredForThePage() {
+
+    }
+
+    @Then("^he finally could subscribe in the platform for testers community$")
+    public void heFinallyCouldSubscribeInThePlatformForTestersCommunity() {
+
+    }
 }
--- src/test/resources/features/signUpInUTest.feature
+++ src/test/resources/features/signUpInUTest.feature
@@ -1,1 +1,9 @@
 #Autor: Alexander Echeverry Torres
+@Stories
+Feature: Sign Up In uTest
+  As an anonymous user, I want to SignUp in uTest for a challenge because I want to work in Choucair
+  @scenario1
+  Scenario: SignUp in uTest
+    Given than Alexander wants to create account in uTest.com
+    When he fill the data required for the page
+    Then he finally could subscribe in the platform for testers community
